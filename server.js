10d9205daa4a6add85f7de38d5a0d7ee62f567f5
zavadskij944 <zavadskij944@gmail.com>
2026-03-31 23:46:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
-const Stripe = require("stripe");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(express.json());
